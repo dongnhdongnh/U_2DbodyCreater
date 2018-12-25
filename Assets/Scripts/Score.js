e9214421e81be0f901761730598ab26046870faa
dongnh <dongnh@gemmob.com>
2018-12-25 14:07:41 +0700

    1st

---
+++ Assets/Scripts/Score.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+static var playerScore : int = 0;
+
+static function score() {
+	playerScore += 1;
+}
+
+static function resetScore() {
+	playerScore = 0;
+}
+
+static function getScore() {
+	return playerScore;
+}
---
+++ Assets/Scripts/icicleController.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+static var finalScore : int = 0;
+function Start () {
+	var myRigidBody : Rigidbody2D = GetComponent(Rigidbody2D);
+		myRigidBody.gravityScale = Random.Range(0.01,0.5);
+}
+
+function OnTriggerExit2D (coll : Collider2D) {
+		if(coll.gameObject.tag == 'bottomWall') {
+			Score.score();
+			Destroy(this.gameObject);
+		} else if (coll.gameObject.tag == 'limb'){
+			Destroy(coll.gameObject);
+		} else if (coll.gameObject.tag == 'vital'){
+			finalScore = Score.getScore();
+			Debug.Log(finalScore);
+			Destroy(coll.gameObject);
+			Application.LoadLevel('game');
+			Score.resetScore();
+		}
+		
+}
